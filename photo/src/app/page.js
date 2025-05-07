a5ed02e0981b3617e14e55f9b9172da49f81353e
sksinthu <sksinthu15@gmail.com>
2025-05-07 20:45:21 +0530

    page file update

--- photo/src/app/page.js
+++ photo/src/app/page.js
@@ -3,10 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div className=" heros-section"></div>
+    <div className="experience-section"></div>
+    <div className="explore-section"></div>
+    <div className="creativity-section"></div>
+    <div className="vision-section"></div>
+    <div className="gallery-section"></div>
+    <div className="explore-section"></div>
    </div>
   );
 }
