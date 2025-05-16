80d7c11a1df7de690404ef9bc368e566e8131126
kishanth29 <139776257+kishanth29@users.noreply.github.com>
2025-05-16 11:46:18 +0530

    all fixed

--- photo/src/app/page.js
+++ photo/src/app/page.js
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import Hero from "./components/Sections/Hero";
 import CreativitySection from "./components/Sections/CreativitySection";
-import Grid from "./components/Sections/Grid";
+
 import ExpertiseSection from "./components/Sections/ExpertiseSection";
 import StudioLayout from "./components/Sections/StudioLayout";
 import GalleryPage from "./components/Sections/Gallery";
+import HeroSection from "./components/Sections/Experience";
+import Expertise from "./components/Sections/expect";
 export default function Home() {
   return (
     <div>
       <div className=" heros-section">
         <Hero />
       </div>
-      <div className="experience-section"></div>
-      <div className="explore-section"></div>
+      <div className="experience-section">
+        <HeroSection />
+      </div>
+      <div className="explore-section">
+        <Expertise />
+      </div>
       <div className="creativity-section">
         <CreativitySection />
       </div>
       <div className="vision-section">
-        {/* <Grid /> */}
-        <ExpertiseSection />{" "}
+        <ExpertiseSection />
       </div>
       <div className="gallery-section">
         <GalleryPage />
