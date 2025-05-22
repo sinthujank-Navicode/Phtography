0495f1f31ea85b7146759e1a836e988ed6740de9
sksinthu <sksinthu15@gmail.com>
2025-05-22 16:34:51 +0530

    hero changes

--- photo/src/app/components/Sections/Gallery.js
+++ photo/src/app/components/Sections/Gallery.js
@@ -1,23 +1,23 @@
 import Image from "next/image";
 
 const images = [
-  { src: "/images/gallery1.jpg", classes: "col-span-2" },
-  { src: "/images/gallery8.jpg", classes: "col-start-3 row-start-1" },
+  { src: "/images/gallery1.jpg", classes: "col-span-2 md:h-[18rem]" },
+  { src: "/images/gallery8.jpg", classes: "col-start-3 row-start-1 md:h-[18rem]" },
+  {
+    src: "/images/gallery6.jpg",
+    classes: "row-span-2 col-start-1 row-start-2 md:h-[40.7rem]",
+  },
   {
     src: "/images/gallery9.jpg",
-    classes: "row-span-2 col-start-1 row-start-2",
+    classes: "col-span-2 col-start-2 row-start-2 md:h-[18rem]",
   },
-  {
-    src: "/images/gallery3.jpg",
-    classes: "col-span-2 col-start-2 row-start-2",
-  },
-  { src: "/images/gallery5.jpg", classes: "col-start-2 row-start-3" },
-  { src: "/images/gallery2.jpg", classes: "col-start-3 row-start-3" },
+  { src: "/images/gallery5.jpg", classes: "col-start-2 row-start-3 md:h-[22rem]" },
+  { src: "/images/gallery10.jpg", classes: "col-start-3 row-start-3 md:h-[22rem]" },
 ];
 
 export default function GalleryPage() {
   return (
-    <div className=" md:min-h-screen min-h-[60vh] p-6">
+    <div className=" md:min-h-screen min-h-[60vh] p-6 md:mb-12">
       {/* Header Section */}
       <div className="text-start max-w-2xl mx-auto mb-10">
         <h1 className="text-4xl font-bold text-gray-800 mb-3">
@@ -29,7 +29,7 @@ export default function GalleryPage() {
       </div>
 
       {/* Gallery Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 md:gap-7 gap-1 max-w-7xl mx-auto  h-auto md:h-[90vh]">
+      <div className="grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 md:gap-7 gap-1 max-w-7xl mx-auto  h-auto md:h-[99vh]">
         {images.map((img, index) => (
           <div key={index} className={`${img.classes} p-1`}>
             <Image
